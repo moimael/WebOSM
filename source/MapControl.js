ccc94e22086eb1d3e9c30ef1f4b1a4c83f9aabee
Maël Lavault <mael.lavault@mailz.org>
2012-03-08 19:27:16 +0100

    Added basic routing support, lot of code refactoring

--- source/MapControl.js
+++ source/MapControl.js
@@ -1,3 +1,16 @@
+/*
+A map control which is a wrapper around Leaflet map control.
+
+To initialize a Map control:
+
+	{name: "map", kind: "MapControl", credentials: "my_cloudmade_credentials"}
+	
+You can get a handle to the actual Leaflet map control uisng hasMap(), like this:
+
+	var mapControl = this.$.map.hasMap();
+	
+*/
+
 enyo.kind({
 	name: "WebOSM.MapControl", 
 	kind: "enyo.Control",
@@ -9,17 +22,35 @@ enyo.kind({
 	},
 
 	rendered: function() {
+		this.renderMap();
+	},
+	
+	createMap: function(){
 		this.map = new L.Map(this.hasNode(), {zoomControl : false});
-
-		// create a CloudMade tile layer (or use other provider of your choice)
+		this.layerGroup = new L.LayerGroup();
+	},
+	
+	destroyMap: function(){
+		this.map = null;
+	},
+	
+	renderMap: function() {
+		this.destroyMap();
+		try {
+			this.createMap();
+		} catch (e) {
+//			this.doLoadFailure(e);
+			return;
+		}
+		
+		// create a CloudMade tile layer
 		var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/' + this.credentials + '/997/256/{z}/{x}/{y}.png', {
 			maxZoom: 18
 		});
-		
-//		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
 
-		// add the CloudMade layer to the map set the view to a given center and zoom
+		// add the CloudMade layer to the map and set the view to a given center
 		this.map.addLayer(cloudmade).setView(new L.LatLng(51.505, -0.09), 3);
+		this.map.addLayer(this.layerGroup);
 		this.connectEvents();
 	},
 	
@@ -27,23 +58,32 @@ enyo.kind({
 		return this.map;
 	},
 	
+	hasLayers: function() {
+		return this.layerGroup;
+	},
+	
+	clearAll: function() {
+		this.layerGroup.clearLayers();
+	},
+	
 	connectEvents: function() {
-		var marker;
-		var circle;
-		map = this.map;
-		this.map.on('locationfound', function(e) {
+		this.map.on('locationfound', enyo.bind(this, "gotLocation"));
+		this.map.on('locationerror', enyo.bind(this, "gotLocationError"));
+	},
+	
+	gotLocationError: function(e) {
+			enyo.windows.addBannerMessage("Unable to find your location", '{}');
+	},
+	
+	gotLocation: function(e) {
 			if (typeof marker == 'undefined' && typeof circle == 'undefined'){
 				var radius = e.accuracy / 2;
-				marker = new L.Marker(e.latlng);
-				map.addLayer(marker);
+				var marker = new L.Marker(e.latlng);
+				this.map.addLayer(marker);
 				marker.bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-				circle = new L.Circle(e.latlng, radius);
-				map.addLayer(circle);
+				var circle = new L.Circle(e.latlng, radius);
+				this.map.addLayer(circle);
 			}
-		});
-		this.map.on('locationerror', function(e) {
-			enyo.windows.addBannerMessage("Unable to find your location", '{}');
-		});
 	}
 });
